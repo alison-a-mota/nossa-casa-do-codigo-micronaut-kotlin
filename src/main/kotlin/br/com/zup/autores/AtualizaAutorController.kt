481fe3632b723de7ea5dd82367aac07ae990bc3a
Alison <alison.mota@zup.com.br>
2021-09-18 23:07:12 -0300

    Initial commit

---
+++ src/main/kotlin/br/com/zup/autores/AtualizaAutorController.kt
@@ -0,0 +1,7 @@
+package br.com.zup.autores
+
+import io.micronaut.http.annotation.Controller
+
+@Controller
+class AtualizaAutorController {
+}
---
+++ src/main/kotlin/br/com/zup/autores/CadastraAutorController.kt
@@ -0,0 +1,27 @@
+package br.com.zup.autores
+
+import io.micronaut.http.HttpResponse
+import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Post
+import io.micronaut.http.uri.UriBuilder
+import io.micronaut.validation.Validated
+
+import javax.validation.Valid
+
+@Validated
+@Controller("/autor")
+class CadastraAutorController(val autorRepository: AutorRepository) {
+
+    @Post
+    fun cadastra(@Body @Valid request: NovoAutorRequest): HttpResponse<Any> {
+
+        val autor = request.toModel()
+        autorRepository.save(autor)
+
+        val uri = UriBuilder.of("/autores/{id}")
+            .expand(mutableMapOf(Pair("id", autor.id)))
+
+        return HttpResponse.created(uri)
+    }
+}
